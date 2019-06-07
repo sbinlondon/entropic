2f30db6b23c9c8d18cffd5a566ddbd8dda0afa54
Chris Dickinson <christopher.s.dickinson@gmail.com>
2019-06-06 21:17:27 -0700

    fix: storage runs now

--- services/storage/handlers/index.js
+++ services/storage/handlers/index.js
@@ -4,11 +4,8 @@ module.exports = makeRouter;
 
 const ship = require('culture-ships').random();
 
-const isLoggedIn = require('../decorators/is-logged-in');
 const { response, fork } = require('boltzmann');
 const pkg = require('../package.json');
-const User = require('../models/user');
-const auth = require('./auth');
 
 function makeRouter() {
   const router = fork.router()(
@@ -17,9 +14,6 @@ function makeRouter() {
     ...require('./maintainers'),
     ...require('./namespaces'),
 
-    fork.get('/-/v1/login/poll/:session', auth.poll),
-    fork.post('/-/v1/login', auth.login),
-    fork.get('/v1/auth/whoami', isLoggedIn(whoami)),
     fork.get('/ping', ping)
   );
 
@@ -36,22 +30,6 @@ async function version() {
   return response.json(data);
 }
 
-async function greeting() {
-  const objects = await User.objects.all().then();
-  return response.json({ objects });
-}
-
 async function ping() {
   return response.text(ship);
-}
-
-async function whoami(context) {
-  if (!context.user) {
-    return response.error({
-      message: 'You are not logged in',
-      CODE: 'ENOTLOGGEDIN'
-    });
-  }
-  // This isn't to spec but is what vcpm does. Consider changing it.
-  return response.json({ username: context.user.name });
 }
